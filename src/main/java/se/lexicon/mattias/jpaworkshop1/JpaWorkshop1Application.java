27f1efa4f51b2216ed8ab15262e35b5d46ed9fd9
Mattias Andersson <neonithe@gmail.com>
2020-08-12 09:41:13 +0200

    Initial commit, all setup done. Spring H2 test MYSQL DB Config, H2 and MYSQL

---
+++ src/main/java/se/lexicon/mattias/jpaworkshop1/JpaWorkshop1Application.java
@@ -0,0 +1,13 @@
+package se.lexicon.mattias.jpaworkshop1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaWorkshop1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaWorkshop1Application.class, args);
+	}
+
+}
---
+++ src/test/java/se/lexicon/mattias/jpaworkshop1/JpaWorkshop1ApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.mattias.jpaworkshop1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaWorkshop1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
